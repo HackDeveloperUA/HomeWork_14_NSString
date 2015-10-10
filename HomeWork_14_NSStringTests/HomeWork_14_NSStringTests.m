ebf91fad4c78924273ad140351136b08ddb02a89
HackDeveloperUA <devios161@gmail.com>
2015-10-10 18:16:23 +0300

    First Commit
    String

---
+++ HomeWork_14_NSStringTests/HomeWork_14_NSStringTests.m
@@ -0,0 +1,40 @@
+//
+//  HomeWork_14_NSStringTests.m
+//  HomeWork_14_NSStringTests
+//
+//  Created by MD on 27.02.15.
+//  Copyright (c) 2015 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HomeWork_14_NSStringTests : XCTestCase
+
+@end
+
+@implementation HomeWork_14_NSStringTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
